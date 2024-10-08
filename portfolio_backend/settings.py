ee085366551b971fc28554e40bbcf575fc7dec29
hiba <>
2024-10-09 00:41:19 +0200

    update env

--- portfolio_backend/settings.py
+++ portfolio_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-secret-key')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['https://django-profile.onrender.com', 'localhost']
